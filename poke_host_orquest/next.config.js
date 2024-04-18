7a14b5cae8a58c0f17ce6f81921bf3e13e882c42
bdanielmr <bdanielmr@gmail.com>
2024-04-17 23:21:43 -0500

    chore: add remote federate modules on orquest

--- poke_host_orquest/next.config.js
+++ poke_host_orquest/next.config.js
@@ -3,10 +3,15 @@ const remotes = (isServer) => {
   const location = isServer ? "ssr" : "chunks";
   return {
     poke_remote_1: `poke_remote_1@${process.env.NEXT_PUBLIC_REMOTE_BASE_URL}/_next/static/${location}/remoteEntry.js`,
+    poke_remote_2: `poke_remote_2@${process.env.NEXT_PUBLIC_REMOTE_2_BASE_URL}/_next/static/${location}/remoteEntry.js`,
+    poke_remote_3: `poke_remote_3@${process.env.NEXT_PUBLIC_REMOTE_3_BASE_URL}/_next/static/${location}/remoteEntry.js`,
   };
 };
 const nextConfig = {
-  reactStrictMode: true,
+  images: {
+    domains: ['raw.githubusercontent.com'],
+  },
+  reactStrictMode: false,
   webpack(config, options) {
     config.plugins.push(
       new NextFederationPlugin({
