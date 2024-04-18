2e9065ad5c028f6d7193b951293118de30824204
bdanielmr <bdanielmr@gmail.com>
2024-04-17 23:19:35 -0500

    feat: add poke_remote_2 module federate

---
+++ poke_remote_2/next.config.js
@@ -0,0 +1,31 @@
+const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
+const remotes = (isServer) => {
+  const location = isServer ? "ssr" : "chunks";
+  return {
+    poke_host_orquest: `poke_host_orquest@${process.env.NEXT_PUBLIC_ORQUEST_BASE_URL}/_next/static/${location}/remoteEntry.js`,
+  };
+};
+const nextConfig = {
+  images: {
+    domains: ['raw.githubusercontent.com'],
+  },
+  reactStrictMode: false,
+  webpack(config, options) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "poke_remote_2",
+        filename: "static/chunks/remoteEntry.js",
+        remotes: remotes(options.isServer),
+        exposes: {
+        },
+        extraOptions: {
+          exposePages: true,
+          automaticAsyncBoundary: true,
+        },
+      })
+    );
+    return config;
+  },
+};
+
+module.exports = nextConfig;
---
+++ poke_remote_2/src/pages/index.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useRef } from 'react';
+import Head from "next/head";
+import Image from "next/image";
+import dynamic from 'next/dynamic';
+
+  // @ts-ignore
+  const PokeCard = dynamic(() => import('poke_host_orquest/PokeCard'), {
+    loading: () => <p>Loading...</p>, // Componente a mostrar mientras se carga
+    ssr: false // Permite que este componente sea renderizado solo en el cliente
+  });
+
+const Home = ({ choose }: { choose: { pokemon: string } }) => {
+  const defaultChoose = choose || { pokemon: 'charmander' };
+
+  return (
+    <>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        {/* @ts-ignore*/}
+        <PokeCard choose={defaultChoose} url={`https://pokeapi.co/api/v2/pokemon/${defaultChoose.pokemon}`} />
+      </div>
+    </>
+  );
+};
+
+export default Home;
+
